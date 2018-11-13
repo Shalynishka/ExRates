a6cc0d4bbbd1cdc7549104d5f74e74b308901aa9
Kaleko Nikita, 650501 <36746354+Shalynishka@users.noreply.github.com>
2018-11-14 02:29:44 +0300

    Add files via upload

---
+++ app/currency.py
@@ -0,0 +1,57 @@
+﻿from item import Item
+
+
+class Currency(Item):
+    """
+        Class for Currency. It contains Currency information.
+        Child from Item
+
+        Attributes:
+            __symbol - symbol of Currency
+            __fav    - 'favorite' status
+
+        Methods and properties:
+
+            __init__(self, name, s_n, icon, rate, date, s, f)
+            __dict__(self) - return all info in dict
+
+            properties(getter~+setter):
+                symbol
+                fav
+
+        """
+    __symbol = ''
+    __fav_status = False
+
+    # (self, name: str, short: str, icon, rate: float, date: datetime.date, symbol: str, fav: bool):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__symbol = kwargs['symbol']
+        self.__fav_status = kwargs['fav']
+
+    """AKA get info"""
+    def __dict__(self) -> dict:
+        return {'name': self.name,
+                'short': self.short,
+                'rate': self.rate,
+                'date': self.date,
+                'symbol': self.symbol,
+                'fav': self.fav_s}
+
+    """symbol of currency"""
+    @property
+    def symbol(self):
+        return self.__symbol
+
+    @symbol.setter
+    def symbol(self, s):
+        self.__symbol = s
+
+    """favorite status"""
+    @property
+    def fav_s(self):
+        return self.__fav_status
+
+    @fav_s.setter
+    def fav_s(self, f):
+        self.__fav_status = f
---
+++ app/item.py
@@ -0,0 +1,79 @@
+﻿class Item:
+    """
+    Class for Item. It contains general characteristics for Currencies and Resources
+    Attributes:
+        __name   - name of Item
+        __short - short name of Item
+        __icon   - mini image of Item
+        __rate   - rate of Item [1 $ to 2.1431 by]
+        __date   - date of last update of Item
+
+    Methods and properties:
+
+        __init__(self, name, s_n, icon, rate, date)
+
+        properties(getter~+setter):
+            name
+            s_name
+            rate
+            date
+
+        todo: Icon and work with it
+    """
+    __name = ''
+    __short = ''
+    __icon = 'Some'   # по имени подгружать иконку? Тогда не нужен конструктор
+    __rate = .0
+    __date = ''
+
+    def __init__(self, **kwargs):   # (name: str, s_n: str, icon, rate: float, date: datetime.date)
+        self.__name = kwargs['name']
+        self.__short = kwargs['short']
+        self.__icon = kwargs['short'] + '.atr'
+        self.__rate = kwargs['rate']
+        self.__date = kwargs['date']
+
+    """name of item"""
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, n):
+        self.__name = n
+
+    """short name of item"""
+    @property
+    def short(self):
+        return self.__short
+
+    @short.setter
+    def s_name(self, sn):
+        self.__short = sn
+
+    """rate of item"""
+    @property
+    def rate(self):
+        return self.__rate
+
+    @rate.setter
+    def rate(self, r):
+        self.__rate = r
+
+    """date of last update"""
+    @property
+    def date(self):
+        return self.__date
+
+    @date.setter
+    def date(self, d):
+        self.__date = d
+
+    """icon"""
+    @property
+    def icon(self):
+        return self.__icon
+
+    @icon.setter
+    def icon(self, i):
+        self.__icon = i
